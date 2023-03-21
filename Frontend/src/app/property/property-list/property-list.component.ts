2897821611f739e0766594b074517d1abd5015b1
Saleem <dellprec213@gmail.com>
2023-03-21 21:07:52 +0500

    Animation_Property_Cards

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> =[
+  {
+      "Id": 1,
+      "Name":"Saleem House",
+      "Type": "House",
+      "Price": 12000
+  },
+  {
+    "Id": 1,
+    "Name":"Saleem House",
+    "Type": "House",
+    "Price": 12000
+  },
+  {
+  "Id": 2,
+  "Name":"Kaka Villa",
+  "Type": "Villa",
+  "Price": 11000
+  },
+  {
+    "Id": 3,
+    "Name":"Solitude",
+    "Type": "Solitude Standard",
+    "Price": 13000
+  },
+]
+
   constructor() { }
+
 
   ngOnInit() {
   }
